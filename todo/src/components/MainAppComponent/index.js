b83405696197f2ab9806a0dc5d516e209c62f9dc
Sykpen <ser10potap@yandex.ru>
2021-04-13 11:16:27 +0300

    feat: add CRA template, delete all useless info

---
+++ todo/src/components/MainAppComponent/index.js
@@ -0,0 +1,11 @@
+import styles from './style.module.css'
+
+function App() {
+    return (
+      <div className={styles.test_class}>
+          <p>test</p>
+      </div>
+    );
+  }
+  
+  export default App;
